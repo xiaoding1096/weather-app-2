ca5413b7fefa7cbeffb6bc703afffc272465318e
xiaoding1096 <hoangnguyendt1096@gmail.com>
2024-11-19 21:08:05 +0100

    i have a questtion

--- main.js
+++ main.js
@@ -2,29 +2,58 @@
 const dayOfFuture = document.getElementById("dayoffuture-wrapper");
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
-// we
-let data = ["day 1","day 2","day 3","day 4","day 5","day 6","day 7",]
+const todayCity = document.getElementById("today-city");
+const todayTemperature = document.querySelector(".today-temperature");
+const dayoffutureDay = document.querySelector(".dayoffuture-day");
+
 //line 8 to line 13 will help don't reload page when we submit,return a null value in input when we have written something and run a function displayWeather
 searchForm.addEventListener("submit", searchInfoWeatherCity);
 function searchInfoWeatherCity(event) {
   event.preventDefault();
+
+  displayWeatherToday();
+  // displayWeatherNext8days();
   searchInput.value = "";
-  displayWeather();
+}
+// this function will get information from api and render to object "today" in html
+async function displayWeatherToday() {
+  let citySearch = searchInput.value.trim();
+  let apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${citySearch}?key=579XEPE66XU8KA92CEUPVGEE9 `;
+
+  let data = await fetch(apiURL)
+    .then((response) => response.json())
+    .catch((error) => {
+      alert("Something Went Wrong: Try Checking Your Internet Coneciton");
+    });
+  console.log(data);
+
+  todayCity.innerHTML = data.resolvedAddress;
+  todayTemperature.innerHTML = data.currentConditions.temp;
 }
 
 //this function allow we display information when we have written something to input through create a new object, add a class for that object and add html content in that
-function displayWeather() {
-    data.forEach( data => {
+async function displayWeatherNext8days() {
+  let citySearch = searchInput.value.trim();
+  let apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${citySearch}?key=579XEPE66XU8KA92CEUPVGEE9 `;
+
+  let data = await fetch(apiURL)
+    .then((response) => response.json())
+    .catch((error) => {
+      alert("Something Went Wrong: Try Checking Your Internet Coneciton");
+    });
+  // anh ơi em muốn hỏi ở chỗ này đi xuống em không biết cách tham chiếu như thế nào để api có thể tham chiếu vào đây
+  data.days.forEach((day) => {
     const newDivItem = document.createElement("div");
     newDivItem.classList.add("dayoffuture_day");
+
     newDivItem.innerHTML = `
-        <span class="dayoffuture-day">${data}</span>
+    <span class="dayoffuture-day"></span> 
         <img src="./img/sun.png" alt="" class="img-dayoffuture_icon" />
         <div class="dayoffuture-minmax-temperature">
-        <span class="dayoffuture-minmax">10</span>
-        <span class="dayoffuture-minmax">30</span>
+        <span class="dayoffuture-minmax">${data.days[0].tempmin}</span>
+        <span class="dayoffuture-minmax">${data.days[0].tempmax}</span>
         </div>
     `;
     dayOfFuture.appendChild(newDivItem);
-    });
+  });
 }
