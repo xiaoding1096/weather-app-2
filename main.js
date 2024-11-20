7c7947afaf9e79b5c7c7c9664d014338901ee4cd
xiaoding1096 <hoangnguyendt1096@gmail.com>
2024-11-20 21:33:17 +0100

    base version 2

--- main.js
+++ main.js
@@ -5,19 +5,25 @@ const searchInput = document.getElementById("search-input");
 const todayCity = document.getElementById("today-city");
 const todayTemperature = document.querySelector(".today-temperature");
 const dayoffutureDay = document.querySelector(".dayoffuture-day");
-
+const conditions = document.querySelector(".conditions-info");
+const feelslike = document.querySelector(".feelslike-info");
+const humidity = document.querySelector(".humidity-info");
+const visibility = document.querySelector(".visibility-info");
+const sunrise = document.querySelector(".sunrise-info");
+const sunset = document.querySelector(".sunset-info");
 //line 8 to line 13 will help don't reload page when we submit,return a null value in input when we have written something and run a function displayWeather
 searchForm.addEventListener("submit", searchInfoWeatherCity);
 function searchInfoWeatherCity(event) {
   event.preventDefault();
 
   displayWeatherToday();
-  // displayWeatherNext8days();
+  displayWeatherNext8days();
   searchInput.value = "";
 }
 // this function will get information from api and render to object "today" in html
 async function displayWeatherToday() {
   let citySearch = searchInput.value.trim();
+
   let apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${citySearch}?key=579XEPE66XU8KA92CEUPVGEE9 `;
 
   let data = await fetch(apiURL)
@@ -26,34 +32,51 @@ async function displayWeatherToday() {
       alert("Something Went Wrong: Try Checking Your Internet Coneciton");
     });
   console.log(data);
-
+  conditions.innerHTML = data.currentConditions.conditions;
+  feelslike.innerHTML = Math.round((data.currentConditions.feelslike - 32) * 5/9);
+  humidity.innerHTML = data.currentConditions.humidity + " %";
+  visibility.innerHTML = data.currentConditions.visibility + " K/m";
+  sunrise.innerHTML = data.currentConditions.sunrise;
+  sunset.innerHTML = data.currentConditions.sunset;
   todayCity.innerHTML = data.resolvedAddress;
-  todayTemperature.innerHTML = data.currentConditions.temp;
+  todayTemperature.innerHTML = Math.round((data.currentConditions.temp - 32) * 5/9);
 }
 
 //this function allow we display information when we have written something to input through create a new object, add a class for that object and add html content in that
 async function displayWeatherNext8days() {
   let citySearch = searchInput.value.trim();
-  let apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${citySearch}?key=579XEPE66XU8KA92CEUPVGEE9 `;
+
+  let apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline//${citySearch}?key=579XEPE66XU8KA92CEUPVGEE9&icons1`;
 
   let data = await fetch(apiURL)
     .then((response) => response.json())
     .catch((error) => {
       alert("Something Went Wrong: Try Checking Your Internet Coneciton");
     });
-  // anh ơi em muốn hỏi ở chỗ này đi xuống em không biết cách tham chiếu như thế nào để api có thể tham chiếu vào đây
+
   data.days.forEach((day) => {
     const newDivItem = document.createElement("div");
     newDivItem.classList.add("dayoffuture_day");
 
     newDivItem.innerHTML = `
-    <span class="dayoffuture-day"></span> 
-        <img src="./img/sun.png" alt="" class="img-dayoffuture_icon" />
+    <span class="dayoffuture-day">${day.datetime}</span> 
+        <img src="${day.icon}" alt="" class=" img-dayoffuture_icon" />
         <div class="dayoffuture-minmax-temperature">
-        <span class="dayoffuture-minmax">${data.days[0].tempmin}</span>
-        <span class="dayoffuture-minmax">${data.days[0].tempmax}</span>
+        <div>
+        <span>Min</span>
+        <span class="dayoffuture-minmax">${day.tempmin}</span>
+        </div>
+        <div>
+        <span>Max</span>
+        <span class="dayoffuture-minmax">${day.tempmax}</span>
+        </div>
         </div>
     `;
+
     dayOfFuture.appendChild(newDivItem);
   });
 }
+
+//add day time
+// add user position make that is default when dont have input
+//add info have not finished
