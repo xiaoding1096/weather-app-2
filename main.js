77a86948d322a7a24ff83d00ec6b21c2d84e9cee
xiaoding1096 <hoangnguyendt1096@gmail.com>
2024-11-21 22:12:32 +0100

    add feature display user's location

--- main.js
+++ main.js
@@ -11,20 +11,82 @@ const humidity = document.querySelector(".humidity-info");
 const visibility = document.querySelector(".visibility-info");
 const sunrise = document.querySelector(".sunrise-info");
 const sunset = document.querySelector(".sunset-info");
-//line 8 to line 13 will help don't reload page when we submit,return a null value in input when we have written something and run a function displayWeather
+// let userLocationAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a1a59d2f73eeb4a3e6a24a861af886e4`;
+//This function will help don't reload page when we submit,return a null value in input when we have written something and run a function displayWeather
 searchForm.addEventListener("submit", searchInfoWeatherCity);
 function searchInfoWeatherCity(event) {
   event.preventDefault();
-
-  displayWeatherToday();
-  displayWeatherNext8days();
-  searchInput.value = "";
+    getUserLocation();
+    displayWeatherToday();
+    displayWeatherNext8days();
+    searchInput.value = "";
 }
+const getLocation = document.getElementById("change-temperature");
+getLocation.addEventListener("click", getUserLocation);
+function getUserLocation() {
+  const success = async (position) => {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log(lat + " " + lon);
+    userLocationAPI = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}?key=579XEPE66XU8KA92CEUPVGEE9`;
+
+    let userData = await fetch(userLocationAPI)
+      .then((response) => response.json())
+      .catch((error) =>
+        alert("Something Went Wrong: Try Checking Your Internet Coneciton")
+      );
+    console.log(userData);
+
+    conditions.innerHTML = userData.currentConditions.conditions;
+    feelslike.innerHTML = Math.round(
+      ((userData.currentConditions.feelslike - 32) * 5) / 9
+    );
+    todayTemperature.innerHTML =
+      Math.round(((userData.currentConditions.temp - 32) * 5) / 9) + "°C";
+    humidity.innerHTML = userData.currentConditions.humidity + " %";
+    visibility.innerHTML = userData.currentConditions.visibility + " K/m";
+    sunrise.innerHTML = userData.currentConditions.sunrise;
+    sunset.innerHTML = userData.currentConditions.sunset;
+    todayCity.innerHTML = userData.resolvedAddress;
+
+    dayOfFuture.innerText = "";
+  userData.days.forEach((day) => {
+    const newDivItem = document.createElement("div");
+    newDivItem.classList.add("dayoffuture_day");
+    newDivItem.innerHTML = `
+    <span class="dayoffuture-day">${day.datetime}</span> 
+        <img src="${day.icon}" alt="" class=" img-dayoffuture_icon" />
+        <div class="dayoffuture-minmax-temperature">
+        <div>
+        <span>Min</span>
+        <span class="dayoffuture-minmax">${Math.round(
+          ((day.tempmin - 32) * 5) / 9
+        )}°C</span>
+        </div>
+        <div>
+        <span>Max</span>
+        <span class="dayoffuture-minmax">${Math.round(
+          ((day.tempmax - 32) * 5) / 9
+        )}°C</span>
+        </div>
+        </div>
+    `;
+
+    dayOfFuture.appendChild(newDivItem);
+  });
+  };
+
+  function error() {
+    alert("Can't support for you");
+  }
+  navigator.geolocation.getCurrentPosition(success);
+}
+
 // this function will get information from api and render to object "today" in html
 async function displayWeatherToday() {
   let citySearch = searchInput.value.trim();
 
-  let apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${citySearch}?key=579XEPE66XU8KA92CEUPVGEE9 `;
+  let apiURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${citySearch}?key=579XEPE66XU8KA92CEUPVGEE9`;
 
   let data = await fetch(apiURL)
     .then((response) => response.json())
@@ -42,9 +104,8 @@ async function displayWeatherToday() {
   sunrise.innerHTML = data.currentConditions.sunrise;
   sunset.innerHTML = data.currentConditions.sunset;
   todayCity.innerHTML = data.resolvedAddress;
-  todayTemperature.innerHTML = Math.round(
-    ((data.currentConditions.temp - 32) * 5) / 9
-  ) + "°C";
+  todayTemperature.innerHTML =
+    Math.round(((data.currentConditions.temp - 32) * 5) / 9) + "°C";
 }
 
 //this function allow we display information when we have written something to input through create a new object, add a class for that object and add html content in that
