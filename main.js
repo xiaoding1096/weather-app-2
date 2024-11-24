440b031d4749fe2d01be80e0df567ba18f934fdd
xiaoding1096 <hoangnguyendt1096@gmail.com>
2024-11-24 10:40:38 +0100

    add function convert temperature

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ const dayOfFutureMinMax = document.querySelector(".dayoffuture-minmax");
 searchForm.addEventListener("submit", searchInfoWeatherCity);
 
 // function searchInfoWeatherCity() will excuted :
-
 function searchInfoWeatherCity(event) {
   event.preventDefault(); // 1. not reload a page when submit
   displayWeatherToday(); // 2. run function displayWeatherToday()
@@ -49,16 +48,14 @@ function getUserLocation() {
 
     // use that information and change to html
     conditions.innerHTML = userData.currentConditions.conditions;
-    feelslike.innerHTML =
-      Math.round(((userData.currentConditions.feelslike - 32) * 5) / 9) + "°C";
-    todayTemperature.innerHTML =
-      Math.round(((userData.currentConditions.temp - 32) * 5) / 9) + "°C";
+    feelslike.innerHTML = convertToday(userData.currentConditions.feelslike);
+    todayTemperature.innerHTML = convertToday(userData.currentConditions.temp);
     humidity.innerHTML = userData.currentConditions.humidity + " %";
     visibility.innerHTML = userData.currentConditions.visibility + " K/m";
     sunrise.innerHTML = userData.currentConditions.sunrise;
     sunset.innerHTML = userData.currentConditions.sunset;
     todayCity.innerHTML = "Weather Of Your Location";
-    iconWeatherToday.src = `./icon/${userData.currentConditions.icon}.png`
+    iconWeatherToday.src = `./icon/${userData.currentConditions.icon}.png`;
     dayOfFuture.innerText = ""; // return content to "" before create a new div with new content inside
 
     //this method allow create a new div and new content
@@ -71,15 +68,14 @@ function getUserLocation() {
         <div class="dayoffuture-minmax-temperature">
         <div>
         <span>Min</span>
-        <span class="dayoffuture-minmax">${Math.round(
-          ((day.tempmin - 32) * 5) / 9
-        )}°C</span>
+        <span class="dayoffuture-minmax">${convertNext15Days(day.tempmin)}
+        </span>
         </div>
         <div>
         <span>Max</span>
-        <span class="dayoffuture-minmax">${Math.round(
-          ((day.tempmax - 32) * 5) / 9
-        )}°C</span>
+        <span class="dayoffuture-minmax">${convertNext15Days(
+          day.tempmax
+        )}</span>
         </div>
         </div>
     `;
@@ -108,15 +104,13 @@ async function displayWeatherToday() {
     });
   console.log(data);
   conditions.innerHTML = data.currentConditions.conditions;
-  feelslike.innerHTML =
-    Math.round(((data.currentConditions.feelslike - 32) * 5) / 9) + "°C";
+  feelslike.innerHTML = convertToday(data.currentConditions.feelslike);
   humidity.innerHTML = data.currentConditions.humidity + " %";
   visibility.innerHTML = data.currentConditions.visibility + " K/m";
   sunrise.innerHTML = data.currentConditions.sunrise;
   sunset.innerHTML = data.currentConditions.sunset;
   todayCity.innerHTML = data.resolvedAddress;
-  todayTemperature.innerHTML =
-    Math.round(((data.currentConditions.temp - 32) * 5) / 9) + "°C";
+  todayTemperature.innerHTML = convertToday(data.currentConditions.temp);
 }
 
 //this function allow we display information of next 15 days when we have written location to input and then create a new object, add a class for that object and add html content in that
@@ -143,18 +137,40 @@ async function displayWeatherNext15days() {
         <div class="dayoffuture-minmax-temperature">
         <div>
         <span>Min</span>
-        <span class="dayoffuture-minmax">${Math.round(
-          ((day.tempmin - 32) * 5) / 9
-        )}°C</span>
+        <span class="dayoffuture-minmax">
+          ${convertNext15Days(day.tempmin)}
+        </span>
         </div>
         <div>
         <span>Max</span>
-        <span class="dayoffuture-minmax">${Math.round(
-          ((day.tempmax - 32) * 5) / 9
-        )}°C</span>
+        <span class="dayoffuture-minmax">${convertNext15Days(
+          day.tempmax
+        )}</span>
         </div>
         </div>
     `;
     dayOfFuture.appendChild(newDivItem);
   });
+}
+
+// This function for convert temperature in section today and use this function in function displayWeatherToday()
+function convertToday(temp) {
+  let message = "";
+  if (changeTemperature.value == "°C") {
+    message = Math.round(((temp - 32) * 5) / 9) + "°C";
+  } else {
+    message = Math.round(temp) + "°F";
+  }
+  return message;
+}
+
+// This function for convert temperature in section next15days and use this function in function displayWeatherNext15days()
+function convertNext15Days(temp) {
+  let message = "";
+  if (changeTemperature.value == "°C") {
+    message = Math.round(((temp - 32) * 5) / 9) + "<span>" + "°C" + "</span>";
+  } else {
+    message = Math.round(temp) + "<span>" + "°F" + "</span>";
+  }
+  return message;
 }
